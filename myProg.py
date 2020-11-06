058657b31682b8443c71d65328e16f5563bcb995
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2020-11-06 17:34:39 +0530

    Update myProg.py

--- myProg.py
+++ myProg.py
@@ -1,17 +1,16 @@
-import re
+import re # importiing the regular expression library
 
-logFile = r"D:\Programming\Regex\SampleIBMLog.txt"
+logFile = r"D:\Programming\Regex\SampleIBMLog.txt" #Location of log file on my device
 
-
+# Function to read the lines of the log file
 def readLines(logFile):
     with open(logFile) as f:
         for line in f:
-            yield line
+            yield line # yeild is to prevent clogging up of memory
 
 
 for line in readLines(logFile):
     mObj = re.match(r'(\d{2}/\d{2}) (\d{2}:\d{2}:\d{2}) warning:(.*)', line, re.I)
-    #mObj = re.match(r'(\d{2}/\d{2}) (\d{2}:\d{2}:\d{2}) warning:(.*)', line, re.I)
     if mObj:
         print(line.strip())
         print(mObj.group(1), '->', mObj.group(2), '->', mObj.group(3))
